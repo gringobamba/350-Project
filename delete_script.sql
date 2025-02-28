f97097072820caafdc3261d007d1c31a040e29d2
Will Hartman <144253925+WillDaBeast78@users.noreply.github.com>
2025-02-28 16:31:14 -0700

    Create delete_script.sql

---
+++ delete_script.sql
@@ -0,0 +1,20 @@
+-- Delete data from ComposedOf table
+DELETE FROM ComposedOf WHERE RoutineID = 1 AND WorkoutID = 1;
+
+-- Delete data from Ingredient table
+DELETE FROM Ingredient WHERE IngredID = 1;
+
+-- Delete data from Drink table
+DELETE FROM Drink WHERE DrinkID = 1;
+
+-- Delete data from Routine table
+DELETE FROM Routine WHERE RoutineID = 1;
+
+-- Delete data from Meal table
+DELETE FROM Meal WHERE MealID = 1;
+
+-- Delete data from Workout table
+DELETE FROM Workout WHERE WorkoutID = 1;
+
+-- Delete data from User table
+DELETE FROM User WHERE UserID = 1;
