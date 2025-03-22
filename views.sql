e18a3869509167523425ced8b213b916889d927e
Jordan Payne <148146113+gringobamba@users.noreply.github.com>
2025-03-22 17:02:06 -0600

    Adding user profile view

--- views.sql
+++ views.sql
@@ -1,1 +1,13 @@
+-- View for displaying USER info
 
+CREATE VIEW user_view AS
+SELECT 
+  UserID, 
+  UserFName, 
+  UserLName, 
+  UserEmail, 
+  Gender, 
+  DoB, 
+  Height, 
+  Weight
+FROM User;
