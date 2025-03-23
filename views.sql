17d63dbe269304db547fceaed4cfefa45048d565
Jordan Payne <148146113+gringobamba@users.noreply.github.com>
2025-03-22 20:42:14 -0600

    Update views.sql

--- views.sql
+++ views.sql
@@ -11,3 +11,34 @@ SELECT
   Height, 
   Weight
 FROM User;
+
+-- View for displaying WORKOUT info
+
+CREATE VIEW workout_view AS
+SELECT 
+  w.WorkoutID,
+  c.RoutineID,
+  r.RoutineName,
+  r.DayOfWeek,
+  r.UserID,
+  w.WorkoutType,
+  w.Duration,
+  w.Reps,
+  w.WeightLifted
+FROM Workout w
+JOIN ComposedOf c ON w.WorkoutID = c.WorkoutID
+JOIN Routine r ON c.RoutineID = r.RoutineID;
+
+-- View for displaying WORKOUT ROUTINE info
+
+CREATE VIEW routine_view AS
+SELECT 
+  r.RoutineID,
+  r.RoutineName,
+  r.UserID,
+  COUNT(c.WorkoutID) AS WorkoutCountTotal,
+  SUM(w.Duration) AS TotalDuration
+FROM Routine r
+LEFT JOIN ComposedOf c ON r.RoutineID = c.RoutineID
+LEFT JOIN Workout w ON c.WorkoutID = w.WorkoutID
+GROUP BY r.RoutineID, r.RoutineName, r.UserID;
